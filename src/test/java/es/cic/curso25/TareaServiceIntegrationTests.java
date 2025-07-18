de0b465584eac832de5f005f67076952c6fcbb10
AlvaroG <aayestarain@formacion.cic.es>
2025-07-18 13:56:40 +0200

    ""

--- src/test/java/es/cic/curso25/TareaServiceIntegrationTests.java
+++ src/test/java/es/cic/curso25/TareaServiceIntegrationTests.java
@@ -1,4 +1,22 @@
 package es.cic.curso25;
 
-public class TareaServiceTests {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import es.cic.curso25.service.TareaService;
+
+@SpringBootTest
+public class TareaServiceIntegrationTests {
+
+    @Autowired
+    private TareaService tareaService;
+
+    @Test
+    void testCreate(){
+        String nombreTarea ="TestTarea";
+        
+    }
+
+    
 }
